bc2995e9f0c542d6e0c31b7e17e7db259001f0fb
nuychocipie <129649095+nuychocopie@users.noreply.github.com>
2023-04-03 19:15:33 +0900

    Add files via upload

---
+++ 01기/Team04/Round2/숫자카드2(10816)/bj_10816.java
@@ -0,0 +1,41 @@
+package Round02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class bj_10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        ArrayList<Integer> myCard = new ArrayList<>(); //가지고 있는 카드
+        ArrayList<Integer> card = new ArrayList<>(); //주어지는 카드
+        ArrayList<Integer> frequencyT = new ArrayList<>(); //빈도수 저장
+
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++){
+            myCard.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<M; i++){
+            card.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i=0; i<M; i++){
+            int time = Collections.frequency(myCard, card.get(i));
+            frequencyT.add(time);
+        }
+        for(int num : frequencyT) {
+            System.out.print(num+" ");
+        }
+
+    }
+}

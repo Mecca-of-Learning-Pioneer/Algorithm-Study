a6f65a3e22f07d37948edfbb68e49a2e35503f5a
nuychocipie <129649095+nuychocopie@users.noreply.github.com>
2023-04-03 19:14:41 +0900

    Add files via upload

---
+++ 01기/Team04/Round2/듣보잡(1764)/bj_1764_2.java
@@ -0,0 +1,40 @@
+package Round02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class bj_1764_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        HashSet<String> notH = new HashSet<>();
+        for (int i=0; i<N; i++){
+            notH.add(br.readLine());
+        }
+
+        ArrayList<String> both = new ArrayList<>();
+
+        for (int i=0; i<M; i++){
+            String s = br.readLine();
+            if (notH.contains(s)){
+                both.add(s);
+                count++;
+            }
+        }
+        Collections.sort(both);
+        System.out.println(count);
+        for (String s : both) {
+            System.out.println(s);
+        }
+
+
+
+    }
+}

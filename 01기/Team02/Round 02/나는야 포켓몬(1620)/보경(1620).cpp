058059fb8d547f21a486d68bf9df09a2d7f50918
Bokyung-R <100823426+Bokyung-R@users.noreply.github.com>
2023-04-06 22:20:27 +0900

    Update 보경(1620).cpp

--- 01기/Team02/Round 02/나는야 포켓몬(1620)/보경(1620).cpp
+++ 01기/Team02/Round 02/나는야 포켓몬(1620)/보경(1620).cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 #include<string>
 #include<map>
+#define endl "\n"
 
 using namespace std;
 
 int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
 	int N = 0, M = 0;
 	string name1 = "";
 	map<int, string> num_na;
 	map<string, int> name_nu;
 	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i < N+1; i++) {
 		cin >> name1;
 		num_na[i] = name1;
 		name_nu[name1] = i;
@@ -19,7 +25,7 @@ int main() {
 	for (int a = 0; a < M; a++) {
 		string value = "";
 		cin >> value;
-		if (value[0] <'A') {
+		if (isdigit(value[0]) != 0) {
 			int i = stoi(value);
 			cout << num_na[i] << endl;
 		}

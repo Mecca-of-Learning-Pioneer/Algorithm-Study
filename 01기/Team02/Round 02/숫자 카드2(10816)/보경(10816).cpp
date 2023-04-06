4f387c4627205ca81005b7f5b1dcb122e4112fac
Bokyung-R <100823426+Bokyung-R@users.noreply.github.com>
2023-04-06 22:35:27 +0900

    Update 보경(10816).cpp

--- 01기/Team02/Round 02/숫자 카드2(10816)/보경(10816).cpp
+++ 01기/Team02/Round 02/숫자 카드2(10816)/보경(10816).cpp
@@ -1,23 +1,29 @@
 #include<iostream>
 #include<map>
+#define endl "\n"
 
 using namespace std;
 
 int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
 	int N = 0;
-	map<int,int> map;
+	map<int, int> map;
 	int value = 0;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> value;
-		map[value] = i;
+		map[value]++;
 	}
 
 	int M = 0;
 	cin >> M;
 	for (int i = 0; i < M; i++) {
 		cin >> value;
-		cout << map.count(value) << " ";
+		cout << map[value] << " ";
 	}
 	cout << endl;
 

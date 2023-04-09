c5154e68d887fa4087db94e2114129d6fb8ed5d3
1829122 <100738276+1829122@users.noreply.github.com>
2023-04-09 18:19:42 +0900

    Create 남호(1269).cpp

---
+++ 01기/Team01/Round 02/대칭차집합(1269)/남호(1269).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+#define endl '\n'    // endl 대신 '\n'을 사용
+using namespace std;
+
+
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int n, m;
+	cin >> n >> m;
+	int cnt = 0;
+	map<int, int> mp1;
+	
+
+	for(int i = 0; i < n+m; i++){
+		int s;
+		cin >> s;
+		if (mp1.find(s) != mp1.end()) {
+			mp1.erase(s);
+		}
+		else mp1[s] = 1;
+		
+	
+	}
+	for (auto a : mp1) {
+		cnt++;
+	}
+	cout << cnt << endl;
+	
+	
+	
+	
+}

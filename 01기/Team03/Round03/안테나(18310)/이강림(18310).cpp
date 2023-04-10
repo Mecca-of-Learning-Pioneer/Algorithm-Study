e0bb148a0a57ed5d07b096d128db34dec3b6ff43
FrogRim <127326495+FrogRim@users.noreply.github.com>
2023-04-10 18:00:54 +0900

    Update 이강림(18310).cpp

--- 01기/Team03/Round03/안테나(18310)/이강림(18310).cpp
+++ 01기/Team03/Round03/안테나(18310)/이강림(18310).cpp
@@ -1,56 +1,57 @@
 #include <iostream>
-#include <vector>
+#include<cstdlib>
+#include <vector> 
 #include <algorithm>
-#include <string>
 
 using namespace std;
 
-int main() {
+int main(void) {
+	
+	cout.tie(NULL); 
+	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);	
+	
+	int N,M,idx;
+	
+	//int result = 99999;
+	
+	cin >> N;
+	
+	vector<int> home(N);
+	
+	for(int i = 0; i<N; i++){
+		
+		cin >> M;
+		home[i] = M;
+	
+	}
+	
+	sort(home.begin(),home.end());
+	
+	/*for(int i = 0; i < N; i++){
+		int temp = 0; // 임시값 저장 
+	
+		for(int j = 0; j < N; j++){
+			
+			temp += abs(home[i] - home[j]); // 모든 집 사이 거리를 체크해서 temp값에 넣음 
+		}
+		
+		// temp 총합이 result값보다 작으면 = 최소 거리이면 
+		if(temp < result)
+		{
+			result = temp; // temp값을 result값에 넣는다. 
+			idx = home[i]; // 그리고 그 값을 idx에서 출력한다. 
+		}
+		
+	}*/
+	
+	// 만약 제일 왼쪽집부터 시작하면 오른쪽으로 갈수록 제일 왼쪽에 있는 집을 제외하면 거리가 줄어든다.
+	// 그러다가 중간지점을 넘는 순간부터 왼쪽의 집이 오른쪽의 집보다 많아져서 거리의 총합이 증가한다.
+	// 그래서 결론은 home 벡터의 중간지점이 최소 거리값을 가진다.  
+	idx = home[((home.size() -1) / 2)];
 
-    int t; // 테스트 케이스의 개수
-    cin >> t;
-    
-    // 테스트 케이스마다 결과를 저장할 벡터
-    vector<string> result(t, "YES"); // 초기값을 "YES"로 설정
-    
-    while (t > 0) { // 각 테스트 케이스에 대해
-
-        int n; // 전화번호의 수
-        cin >> n;
-        
-        vector<string> phoneVec; // 전화번호 목록을 저장할 벡터
-        
-        for (int i = 0; i < n; i++) {
-            string phone;
-            cin >> phone;
-            phoneVec.push_back(phone); // 전화번호를 벡터에 추가
-        }
-        
-        sort(phoneVec.begin(), phoneVec.end()); // 전화번호 목록을 정렬
-        
-        for (int k = 0; k < phoneVec.size() - 1; k++) {
-            int currentLen = phoneVec[k].length();
-            int nextLen = phoneVec[k + 1].length();
-            if (currentLen >= nextLen) {
-                continue;
-            }
-            
-            if (phoneVec[k + 1].substr(0, currentLen) == phoneVec[k]) { // 다음 전화번호의 접두어가 현재 전화번호와 같은 경우
-                result[t-1] = "NO"; // 일관성이 없음, 결과를 "NO"로 설정
-               
-            }
-            break;
-            
-        }
-        phoneVec.clear();
-        t--; // 테스트 케이스 개수 감소
-    }
-
-    // 결과 출력
-    for(int i = 0; i < result.size(); i++){
-        cout << result[i] << endl;
-    }
-
-    return 0;
-
+	cout << idx;
+	
+	
+	return 0;
 }

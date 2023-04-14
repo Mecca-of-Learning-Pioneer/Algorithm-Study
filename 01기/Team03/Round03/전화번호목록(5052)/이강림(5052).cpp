b58ee7cce125c8bde6035024894afefa09e83c8f
FrogRim <127326495+FrogRim@users.noreply.github.com>
2023-04-14 15:54:11 +0900

    Update 이강림(5052).cpp

--- 01기/Team03/Round03/전화번호목록(5052)/이강림(5052).cpp
+++ 01기/Team03/Round03/전화번호목록(5052)/이강림(5052).cpp
@@ -1,56 +1,39 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <string>
-
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false); // 입출력 속도 향상을 위한 코드
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     int t; // 테스트 케이스의 개수
     cin >> t;
-    
-    // 테스트 케이스마다 결과를 저장할 벡터
-    vector<string> result(t, "YES"); // 초기값을 "YES"로 설정
-    
-    while (t > 0) { // 각 테스트 케이스에 대해
-
-        int n; // 전화번호의 수
+    while (t--) {
+        int n; // 전화번호의 개수
         cin >> n;
-        
-        vector<string> phoneVec; // 전화번호 목록을 저장할 벡터
-        
+        vector<string> v(n); // 전화번호 목록을 저장할 벡터
         for (int i = 0; i < n; i++) {
-            string phone;
-            cin >> phone;
-            phoneVec.push_back(phone); // 전화번호를 벡터에 추가
+            cin >> v[i];
         }
-        
-        sort(phoneVec.begin(), phoneVec.end()); // 전화번호 목록을 정렬
-        
-        for (int k = 0; k < phoneVec.size() - 1; k++) {
-            int currentLen = phoneVec[k].length();
-            int nextLen = phoneVec[k + 1].length();
-            if (currentLen >= nextLen) {
-                continue;
+        sort(v.begin(), v.end()); // 전화번호 목록을 사전순으로 정렬하기
+        bool consistent = true; // 일관성 여부를 저장할 변수 초기화하기
+        for (int i = 0; i < n - 1; i++) {
+            string cur = v[i]; // 현재 번호
+            string next = v[i + 1]; // 다음 번호
+            if (cur.size() < next.size() && cur == next.substr(0, cur.size())) { // 현재 번호가 다음 번호의 접두어라면
+                consistent = false; // 일관성이 없다고 표시하고 반복문 종료하기
+                break;
             }
-            
-            if (phoneVec[k + 1].substr(0, currentLen) == phoneVec[k]) { // 다음 전화번호의 접두어가 현재 전화번호와 같은 경우
-                result[t-1] = "NO"; // 일관성이 없음, 결과를 "NO"로 설정
-               
-            }
-            break;
-            
         }
-        phoneVec.clear();
-        t--; // 테스트 케이스 개수 감소
+        if (consistent) { // 일관성이 있다면
+            cout << "YES\n";
+        }
+        else { // 일관성이 없다면
+            cout << "NO\n";
+        }
     }
-
-    // 결과 출력
-    for(int i = 0; i < result.size(); i++){
-        cout << result[i] << endl;
-    }
-
     return 0;
-
 }

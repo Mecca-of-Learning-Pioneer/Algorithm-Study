3bb06f874ccc1d76b65f21fb55cb3899aa82b703
dgim9390 <129017072+dgim9390@users.noreply.github.com>
2023-04-15 15:36:40 +0900

    Update 도원(1764).cpp

--- 2023MLPAlgorithm/01기/Team05/Round 02/듣보잡(1764)/도원(1764).cpp
+++ 2023MLPAlgorithm/01기/Team05/Round 02/듣보잡(1764)/도원(1764).cpp
@@ -1,47 +1,42 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false); //입출력 속도 증가 - 시간초과 해결 
+    cin.tie(0); cout.tie(0);
+
     int n, m;
     cin >> n >> m;
 
-    vector<string> unheard(n);
+    vector<string> unheard(n); // 듣도 못한 사람
+    vector<string> result; // 듣보잡 목록
+
     for (int i = 0; i < n; i++) {
         cin >> unheard[i];
     }
 
-    vector<string> unseen(m);
+    sort(unheard.begin(), unheard.end()); // 듣도 못한 사람 목록 정렬
+
+    string name;
     for (int i = 0; i < m; i++) {
-        cin >> unseen[i];
-    }
-
-    sort(unheard.begin(), unheard.end());
-    sort(unseen.begin(), unseen.end());
-
-    vector<string> result;
-
-    int i = 0, j = 0;
-    while (i < n && j < m) {
-        if (unheard[i] < unseen[j]) {
-            i++;
-        }
-        else if (unheard[i] > unseen[j]) {
-            j++;
-        }
-        else {
-            result.push_back(unheard[i]);
-            i++;
-            j++;
+        cin >> name;
+        if (binary_search(unheard.begin(), unheard.end(), name)) {     // unheard 벡터에서 name이라는 문자열을 찾습니다.   begin & end 둘 ek unheard벡터의 시작과 끝을 알리는 반복자.
+            result.push_back(name); // binary_search 함수는 찾은 경우 true를 반환하고, 그렇지 않은 경우 false를 반환
+                                    // name이 unheard 벡터에 있다면 (true), 듣보잡 목록에 추가합니다. 그렇지 않으면 (false), 아무 작업도 하지 않습니다.             
         }
     }
 
-    cout << result.size() << endl;
-    for (string name : result) {
-        cout << name << endl;
+    sort(result.begin(), result.end()); // 듣보잡 목록 정렬
+    int size = result.size();
+
+    cout << size << "\n";
+    for (int i = 0; i < size; i++) {
+        cout << result[i] << "\n"; // 듣보잡 목록 출력
     }
 
     return 0;
-} 
+}

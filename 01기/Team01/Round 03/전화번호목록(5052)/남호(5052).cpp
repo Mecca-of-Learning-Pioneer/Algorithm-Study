df355bbb3674d3758176f1cb683b3fd9c7432d95
1829122 <100738276+1829122@users.noreply.github.com>
2023-05-01 22:45:16 +0900

    Update 남호(5052).cpp

--- 01기/Team01/Round 03/전화번호목록(5052)/남호(5052).cpp
+++ 01기/Team01/Round 03/전화번호목록(5052)/남호(5052).cpp
@@ -1,1 +1,39 @@
-sssss
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while (t--) {
+        int n; 
+        cin >> n;
+        vector<string> v(n); 
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end()); 
+        bool aa = true; 
+        for (int i = 0; i < n - 1; i++) {
+            string cur = v[i]; // 현재 번호
+            string next = v[i + 1]; // 다음 번호
+            if (cur.size() < next.size() && cur == next.substr(0, cur.size())) { 
+                consistent = false; 
+                break;
+            }
+        }
+        if (aa) {
+            cout << "YES\n";
+        }
+        else { 
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}

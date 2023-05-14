c721e3c20d3735d7a2cd4569fdb5d13b4d9208b9
FrogRim <127326495+FrogRim@users.noreply.github.com>
2023-05-14 22:55:35 +0900

    Update 이강림(10828).cpp

--- 01기/Team03/Round05/스택(10828)/이강림(10828).cpp
+++ 01기/Team03/Round05/스택(10828)/이강림(10828).cpp
@@ -1,1 +1,70 @@
-//����
+//스택
+#include <iostream>
+#include <stack>
+#include <algorithm> 
+
+using namespace std;
+
+int main(void) {
+
+    cout.tie(NULL);
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);	//없으면 시간 초과
+
+    stack<int> my_stack;
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        string order;
+        int push_num = -1;
+     
+        cin >> order;
+
+        if (order == "push")
+        {
+            cin >> push_num;
+            my_stack.push(push_num);
+        }
+        else if (order == "pop")
+        {
+            if (!my_stack.empty())
+            {
+                cout << my_stack.top() << endl;
+                my_stack.pop();
+            }
+            else {
+                cout << -1 << endl;
+            }
+            
+        }
+        else if (order == "size")
+        {
+            cout << my_stack.size() << endl;
+        }
+        else if (order == "empty")
+        {
+            if (!my_stack.empty())
+            {
+                cout << 0 << endl;
+            }
+            else {
+                cout << 1 << endl;
+            }
+        }
+        else if (order == "top")
+        {
+            if (!my_stack.empty())
+            {
+                cout << my_stack.top() << endl;
+                
+            }
+            else {
+                cout << -1 << endl;
+            }
+        }
+    }
+    return 0;
+}

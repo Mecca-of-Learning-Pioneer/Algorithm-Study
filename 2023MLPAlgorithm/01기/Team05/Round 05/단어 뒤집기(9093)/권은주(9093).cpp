39da0a92f10a66ee95b9675924297683c36e22af
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-05-18 22:13:12 +0900

    Update 권은주(9093).cpp

--- 2023MLPAlgorithm/01기/Team05/Round 05/단어 뒤집기(9093)/권은주(9093).cpp
+++ 2023MLPAlgorithm/01기/Team05/Round 05/단어 뒤집기(9093)/권은주(9093).cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <vector>
+using namespace std;
 
+stack<char> stacks;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int T;
+	cin >> T;
+	cin.ignore();
+	
+	string str;
+	vector<vector<string>> words;
+	string word;
+
+	for(int i=0; i<T; i++)
+	{
+   		getline(cin, str);
+ 		str += ' ';
+ 		
+		for(int j=0; j<str.length(); j++)
+		{
+			if(str[j]==' ')
+			{
+				while(!stacks.empty())
+				{
+					cout << stacks.top();
+					stacks.pop();
+				}
+				cout << " ";
+			}
+			else
+			{
+				stacks.push(str[j]);
+			}
+		}
+		
+		cout << endl;
+
+	}
+}

78d1d132aab66dac55a9799578e9e676e53f5abc
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-05-18 22:10:57 +0900

    Update 권은주(9012).cpp

--- 2023MLPAlgorithm/01기/Team05/Round 05/괄호(9012)/권은주(9012).cpp
+++ 2023MLPAlgorithm/01기/Team05/Round 05/괄호(9012)/권은주(9012).cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <vector>
+using namespace std;
+stack<char> stacks;
 
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int T;
+	cin >> T;
+	cin.ignore();
+	
+	string str;
+
+	for(int i=0; i<T; i++)
+	{
+		int key = 0;
+   		getline(cin, str);
+ 		
+		for(int j=0; j<str.length(); j++)
+		{
+			if(str[j] == '(')
+			{
+				stacks.push('(');
+			}
+			else
+			{
+				if(stacks.empty())
+				{
+					cout << "NO" << endl;
+					key = -1;
+					break;
+				}
+				else
+				{
+					stacks.pop();
+				}
+			}
+		}
+		
+		if(key != -1)
+		{
+			if(stacks.empty())
+			{
+				cout << "YES" << endl;
+			}
+			else
+			{
+				cout << "NO" << endl;
+			}
+		}
+		
+		
+		// 스택 비우기  
+		while(!stacks.empty())
+		{
+			stacks.pop();
+		}
+		
+	}
+
+}

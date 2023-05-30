d00454063874698b2f7edecc816667a2aa02ed8e
FrogRim <127326495+FrogRim@users.noreply.github.com>
2023-05-30 09:47:05 +0900

    Update 이강림(1038).cpp

---
+++ 01기/Team03/Round06/감소하는 수(1038)/이강림(1038).cpp
@@ -0,0 +1,32 @@
+//풀지못함
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int current_num = 0; //n번째 감소하는 수를 저장할 변수
+
+int main() {
+
+    
+    int n;
+    cin >> n;  // n번째 감소하는 수를 입력받습니다.
+    bool flag = false;
+    int cnt = 0;
+    while (cnt < n)
+    {
+        int num = current_num / 10;
+
+        if(num == 0){
+            
+        }
+        else {
+            int num1
+        }
+    }
+    
+    return 0;
+}
